2e223bd5024e670e53161338937f6ff90f79535e
Carol Pirela <caropi30@gmail.com>
2019-05-14 19:26:45 -0400

    se comentan los logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ $(function(){
 	$('.carousel').carousel('pause');
 	$('[data-toggle="tooltip"]').tooltip();
 	$(".nav-link,.dropdown-item").click(function() {
-        var t = $(this).attr("href");
+        var link = $(this).attr("href");
         $('.active').removeClass('active');
         $("html, body").animate({
             scrollTop: $(t).offset().top - 50
         }, {
             duration: 1e3,
         });
-        $('body').scrollspy({ target: '#menu',offset: $(t).offset().top });
+        $('body').scrollspy({ target: '#menu',offset: $(link).offset().top });
         $(this).parent().addClass('active');
         return false;
     });
@@ -18,14 +18,13 @@ $(function(){
 // alert('Hi!');
 
 window.onload = function () {
-	alert('Hi!');
 	const work = document.getElementById('work');
 	const navItem = document.getElementsByClassName('nav-item');
-	console.log(navItem);
+	//console.log(navItem);
 	work.setAttribute('style', 'background-color: blue;');
 	for (const item in navItem) {
 		if (navItem[item]) {
-			console.log(navItem[item]);
+			//console.log(navItem[item]);
 			navItem[item].setAttribute('style', 'background-color: red;');
 		}
 	}
